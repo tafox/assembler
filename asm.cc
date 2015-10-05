d7c77d8f3e4713bff4db60a3fc2b8fa5aa27b61e
Thomas Fox <thomas.allan.fox@gmail.com>
2015-10-05 15:13:43 -0400

    11/16 on A3P2.

--- asm.cc
+++ asm.cc
@@ -3,16 +3,33 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <stdio.h>
+#include <string>
 // Use only the neeeded aspects of each namespace
 using std::string;
 using std::vector;
 using std::endl;
 using std::cerr;
 using std::cin;
+using std::cout;
 using std::getline;
 using ASM::Token;
 using ASM::Lexer;
+using ASM::Kind;
+using ASM::INT;
+using ASM::HEXINT;
+using ASM::DOTWORD;
 
+class Assembler {
+	public:
+		Assembler(); 
+		void AsmDotWord(Token* token);
+		void OutputMachineCode();
+	private:
+		void AddInstruction(int instruct);
+		vector<int> machine_code_;
+		map<string, int> symbol_table_;
+};
 
 int main(int argc, char* argv[]){
   // Nested vector representing lines of Tokens
@@ -23,6 +40,7 @@ int main(int argc, char* argv[]){
     // Create a MIPS recognizer to tokenize
     // the input lines
     Lexer lexer;
+	Assembler assembler;
     // Tokenize each line of the input
     string line;
     while(getline(cin,line)){
@@ -34,10 +52,18 @@ int main(int argc, char* argv[]){
     vector<vector<Token*> >::iterator it;
     for(it = tokLines.begin(); it != tokLines.end(); ++it){
       vector<Token*>::iterator it2;
-      for(it2 = it->begin(); it2 != it->end(); ++it2){
+	  int i = 0;
+      for(it2 = it->begin(); it2 != it->end(); ++it2, i++){
         cerr << "  Token: "  << *(*it2) << endl;
+	  	if (i == 0 && (*it2)->getKind() == DOTWORD) {
+			++it2;
+        	cerr << "  Token: "  << *(*it2) << endl;
+			assembler.AsmDotWord(*it2);
+			break;
+		}
       }
     }
+	assembler.OutputMachineCode();
 
   } catch(const string& msg){
     // If an exception occurs print the message and end the program
@@ -52,3 +78,26 @@ int main(int argc, char* argv[]){
     }
   }
 }
+
+Assembler::Assembler() {}
+
+void Assembler::AsmDotWord(Token* token) {
+	Kind kind = token->getKind();
+	if (kind == HEXINT || kind == INT) {
+		AddInstruction((int) token->toInt());
+	}
+}
+
+void Assembler::OutputMachineCode() {
+	vector<int>::iterator it;
+	for (it = machine_code_.begin(); it != machine_code_.end(); ++it) {
+		putchar(*it>>24 & 0xff);
+		putchar(*it>>16 & 0xff);
+		putchar(*it>>8 & 0xff);
+		putchar(*it & 0xff);
+	}
+}
+
+void Assembler::AddInstruction(int instruct) {
+	machine_code_.push_back(instruct);
+};

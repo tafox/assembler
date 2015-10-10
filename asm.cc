127d1c571bac4d4d6d65637c795733685b232eaa
Thomas Fox <thomas.allan.fox@gmail.com>
2015-10-09 22:49:52 -0400

    Assembler end of A3.

--- asm.cc
+++ asm.cc
@@ -1,6 +1,7 @@
 #include "kind.h"
 #include "lexer.h"
 #include <vector>
+#include <map>
 #include <string>
 #include <iostream>
 #include <stdio.h>
@@ -9,9 +10,9 @@
 using std::string;
 using std::vector;
 using std::endl;
+using std::map;
 using std::cerr;
 using std::cin;
-using std::cout;
 using std::getline;
 using ASM::Token;
 using ASM::Lexer;
@@ -19,12 +20,20 @@ using ASM::Kind;
 using ASM::INT;
 using ASM::HEXINT;
 using ASM::DOTWORD;
+using ASM::LABEL;
+using ASM::ID;
+using ASM::ERR;
 
 class Assembler {
 	public:
 		Assembler(); 
 		void AsmDotWord(Token* token);
 		void OutputMachineCode();
+		void OutputSymbolTable();
+		bool LookUpLabel(string label);
+		void AddToSymbolTable(string label, int address);
+		void InsertLabelValue(string label, int address);
+		bool error_;
 	private:
 		void AddInstruction(int instruct);
 		vector<int> machine_code_;
@@ -41,6 +50,7 @@ int main(int argc, char* argv[]){
     // the input lines
     Lexer lexer;
 	Assembler assembler;
+	int address = 0;
     // Tokenize each line of the input
     string line;
     while(getline(cin,line)){
@@ -49,21 +59,80 @@ int main(int argc, char* argv[]){
 
     // Iterate over the lines of tokens and print them
     // to standard error
+	//PASS 1
     vector<vector<Token*> >::iterator it;
     for(it = tokLines.begin(); it != tokLines.end(); ++it){
-      vector<Token*>::iterator it2;
-	  int i = 0;
-      for(it2 = it->begin(); it2 != it->end(); ++it2, i++){
-        cerr << "  Token: "  << *(*it2) << endl;
-	  	if (i == 0 && (*it2)->getKind() == DOTWORD) {
-			++it2;
-        	cerr << "  Token: "  << *(*it2) << endl;
-			assembler.AsmDotWord(*it2);
+		if (assembler.error_) {
 			break;
 		}
-      }
+		vector<Token*>::iterator it2;
+	  	int i = 0;
+      	for(it2 = it->begin(); it2 != it->end(); ++it2, i++){
+			if (assembler.error_) {
+				break;
+			}
+      		//cerr << "  Token: "  << *(*it2) << endl;
+			Kind kind = (*it2)->getKind();
+			if ((kind == ERR) || (i == 0 && kind != LABEL && kind != DOTWORD)) {
+				cerr << "ERROR Invalid Token." << endl;
+				assembler.error_ = true;
+				break;
+			} 
+			// deal with a label 
+			if ((*it2)->getKind() == LABEL) {
+				string label = (*it2)->getLexeme();
+				label = label.substr(0, label.size()-1);
+				if (i == 0) { 
+					assembler.AddToSymbolTable(label, address);
+				} else if (i > 0) {
+					// if previous thing was a label add this label too
+					if ((*(it2-1))->getKind() == LABEL) {
+						assembler.AddToSymbolTable(label, address);
+					}
+				}
+			} 
+			// deal with a DOTWORD instruct
+	  		if ((*it2)->getKind() == DOTWORD) {
+				++it2;
+				if (it2 != it->end()) {
+					assembler.AsmDotWord(*it2);
+				} else {
+					cerr << "ERROR .word missing operand." << endl;
+					assembler.error_ = true;
+					break;
+				}
+				if ((it2+1) != it->end()) {
+					cerr << "ERROR Extra Token." << endl;
+					assembler.error_ = true;
+				}
+				address += 4;
+				break;
+			}
+		}
     }
-	assembler.OutputMachineCode();
+	// PASS 2
+	address = 0;
+    for(it = tokLines.begin(); it != tokLines.end(); ++it){
+		if (assembler.error_) {
+			break;
+		}
+		vector<Token*>::iterator it2;
+	  	int i = 0;
+      	for(it2 = it->begin(); it2 != it->end(); ++it2, i++){
+			if ((*it2)->getKind() == DOTWORD) {
+				++it2;
+				if ((*it2)->getKind() == ID) {
+					assembler.InsertLabelValue((*it2)->getLexeme(), address);
+				}
+				address += 4;
+				break;
+			}
+		}
+	}
+	if (!assembler.error_) {
+		assembler.OutputMachineCode();
+		//assembler.OutputSymbolTable();
+	}
 
   } catch(const string& msg){
     // If an exception occurs print the message and end the program
@@ -79,12 +148,17 @@ int main(int argc, char* argv[]){
   }
 }
 
-Assembler::Assembler() {}
+Assembler::Assembler() : error_(false) {}
 
 void Assembler::AsmDotWord(Token* token) {
 	Kind kind = token->getKind();
 	if (kind == HEXINT || kind == INT) {
-		AddInstruction((int) token->toInt());
+		AddInstruction(token->toInt());
+	} else if (kind == ID) {
+		return;
+	} else {
+		cerr << "ERROR Invalid Operand to .word" << endl;
+		error_ = true;
 	}
 }
 
@@ -98,6 +172,42 @@ void Assembler::OutputMachineCode() {
 	}
 }
 
+void Assembler::OutputSymbolTable() {
+	map<string, int>::iterator it;
+	for (it = symbol_table_.begin(); it != symbol_table_.end(); ++it) {
+		cerr << it->first << " " << it->second << endl;
+	}
+}
+
 void Assembler::AddInstruction(int instruct) {
 	machine_code_.push_back(instruct);
 };
+
+bool Assembler::LookUpLabel(string label) {
+	map<string, int>::iterator it;
+	it = symbol_table_.find(label);
+	if (it != symbol_table_.end()) {
+		cerr << "ERROR Duplicate Label." << endl;
+		error_ = true;
+		return true;
+	}
+	return false;
+}
+
+void Assembler::AddToSymbolTable(string label, int address) {
+	if (!LookUpLabel(label)) {
+		symbol_table_[label] = address;
+	}
+}
+
+void Assembler::InsertLabelValue(string label, int address) {
+	map<string, int>::iterator it;
+	it = symbol_table_.find(label);
+	if (it != symbol_table_.end()) {
+		int label_address = it->second;
+		machine_code_.insert(machine_code_.begin() + (address/4), label_address); 
+	} else {
+		cerr << "ERROR Label undefined." << endl;
+		error_ = true;
+	}
+}
